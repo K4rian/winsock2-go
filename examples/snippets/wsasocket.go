705272d892b18589b5b3800851cb24dc06128db1
K4rian <contact@k4rian.com>
2024-07-14 20:37:56 +0200

    Add more API code snippet (10)...
    - wsasocket.go: WSAIoctl

--- examples/snippets/wsasocket.go
+++ examples/snippets/wsasocket.go
@@ -1,19 +1,23 @@
 package snippets
 
 import (
+	"bytes"
 	"log"
 	"os"
+	"time"
+	"unsafe"
 
 	"github.com/K4rian/winsock2-go/ws2"
 )
 
 /*
 Demonstrate the usage of the following functions:
-- WSADuplicateSocketA: Returns a WSAProtocolInfoA structure that can be used to create a new socket descriptor for a shared socket.
-- WSADuplicateSocketW: Returns a WSAProtocolInfoA structure that can be used to create a new socket descriptor for a shared socket.
+- WSADuplicateSocketA:    Returns a WSAProtocolInfoA structure that can be used to create a new socket descriptor for a shared socket.
+- WSADuplicateSocketW:    Returns a WSAProtocolInfoA structure that can be used to create a new socket descriptor for a shared socket.
 - WSAGetOverlappedResult: Retrieves the results of an overlapped operation on the specified socket.
-- WSASocketA: Creates a socket that is bound to a specific transport-service provider.
-- WSASocketW: Creates a socket that is bound to a specific transport-service provider.
+- WSAIoctl:               Controls the mode of a socket.
+- WSASocketA:             Creates a socket that is bound to a specific transport-service provider.
+- WSASocketW:             Creates a socket that is bound to a specific transport-service provider.
 */
 
 // WSAStartup() has to be called before using these functions.
@@ -108,6 +112,81 @@ func ExampleWSAGetOverlappedResult() {
 	log.Printf("ExampleWSAGetOverlappedResult | Transfert:    %d\n", transfert)
 	log.Printf("ExampleWSAGetOverlappedResult | Wait:         %v\n", wait)
 	log.Printf("ExampleWSAGetOverlappedResult | Flags:        %v\n", flags)
+}
+
+func ExampleWSAIoctl() {
+	/*
+		Note: You must run this code with elevated privileges (Administrator) to use RAW sockets on Windows.
+	*/
+	const SIO_RCVALL = 0x98000001
+
+	// Create a raw socket to capture all network packets.
+	socket, err := ws2.Socket(ws2.AF_INET, ws2.SOCK_RAW, ws2.IPPROTO_IP)
+	if socket == ws2.INVALID_SOCKET {
+		log.Printf("ExampleWSAIoctl | Failed to create socket: %v\n", err)
+		return
+	}
+	defer ws2.Shutdown(socket, ws2.SD_BOTH)
+	defer ws2.CloseSocket(socket)
+	defer log.Printf("ExampleWSAIoctl | Socket closed\n")
+
+	// Bind it to the local IP.
+	sockAddr, sockAddrLen := ws2.NewV4SockAddress("192.168.1.1", 0) // Use your local IP address here!
+	ret, err := ws2.Bind(socket, unsafe.Pointer(sockAddr), sockAddrLen)
+	if ret == ws2.SOCKET_ERROR {
+		log.Printf("ExampleWSAIoctl | Socket bind failed: %v\n", err)
+		return
+	}
+
+	// Set the socket to non-blocking mode.
+	iMode := uint32(1)
+	ret, err = ws2.IoctlSocket(socket, ws2.FIONBIO, &iMode)
+	if ret == ws2.SOCKET_ERROR {
+		log.Printf("ExampleWSAIoctl | Failed to set non-blocking mode on socket: %v\n", err)
+		return
+	}
+
+	// Enable promiscuous mode.
+	ioControlCode := uint32(SIO_RCVALL)
+	ioValue := uint32(1)
+	ioValueSize := uint32(4) // DWORD
+	bytesReturned := uint32(0)
+	ret, err = ws2.WSAIoctl(
+		socket,
+		ioControlCode,
+		unsafe.Pointer(&ioValue),
+		ioValueSize,
+		nil,
+		0,
+		&bytesReturned,
+		nil,
+		nil,
+	)
+	if ret == ws2.SOCKET_ERROR {
+		log.Printf("ExampleWSAIoctl | Failed to set promiscuous mode on socket: %v\n", err)
+		return
+	}
+
+	log.Printf("ExampleWSAIoctl | Socket set to promiscuous mode\n")
+
+	// Let's receive some data with a execution time limit of 10s.
+	curTime := time.Now()
+	endTime := curTime.Add(10 * time.Second)
+	recvBuf := make([]byte, 65536)
+	recvBufLen := int32(65536)
+
+	for curTime.Before(endTime) {
+		ret, _ = ws2.Recv(socket, unsafe.Pointer(&recvBuf[0]), recvBufLen, 0)
+
+		// Let's skip the socket errors.
+		if ret != ws2.SOCKET_ERROR {
+			log.Printf("ExampleWSAIoctl | Received raw data (%d bytes): %v\n", ret, bytes.TrimRight(recvBuf, "\x00"))
+		}
+
+		// Sleep for a bit then refresh the current time.
+		<-time.After(10 * time.Millisecond)
+		curTime = time.Now()
+	}
 }
 
 func ExampleWSASocketA() {
